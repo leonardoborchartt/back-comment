d3718abf0a8ca580336f5adf89b523957dd36667
Leonardo Borchartt <leob_b@hotmail.com>
2020-01-06 18:17:23 +0000

    change localdate type and default

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const CommentSchema = new mongoose.Schema({
-    name:String,
-    says:String,
-    localdate1:String,
-    
+    name: String,
+    says: String,
+    localdate1: {
+        type: Date,
+        default: Date.now
+    }
 });
 module.exports = mongoose.model('Comment', CommentSchema);
